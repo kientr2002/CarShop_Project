9682b986624c881ff72c8cf2734f600ddebadd43
kientr2002 <kien.tranvictory@hcmut.edu.vn>
2023-12-12 16:37:29 +0700

    update news model and controller

--- BE/Controller/News_controller.php
+++ BE/Controller/News_controller.php
@@ -3,13 +3,32 @@ header("Access-Control-Allow-Origin: http://localhost:3000");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 header('Content-Type: application/json');
-// require_once "../Model/News-data.php";
-require_once "../Config/connectDB.php";
+require_once "../Model/News_model.php";
 if($_SERVER["REQUEST_METHOD"] == "GET"){
-  $new_id = $_GET['new_id'];
-  $dbConnection = new db();
-  $dbConnection->connect();
-  // $json_new_id = json_encode($new_id);
-  
+  $new = new news();
+  $new->get_new();
+}
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+  $new = new news();
+  $type = $_POST['type'];
+
+  if($type === 'insert'){
+    $description = $_POST['description'];
+    $content = $_POST['content'];
+    $author_name = $_POST['author_name'];
+    $image = $_POST['image'];
+    $new->insert_new_validate ($description, $content, $author_name, $image);
+  } else if ($type === 'delete'){
+    $new_id = $_POST['new_id'];
+    $new->delete_new($new_id);
+  } else if ($type === 'update'){
+    $new_id = $_POST['new_id'];
+    $description = $_POST['description'];
+    $content = $_POST['content'];
+    $author_name = $_POST['author_name'];
+    $image = $_POST['image'];
+    $new->update_new($new_id, $description, $content, $author_name, $image);
+  }
 }
 ?>
