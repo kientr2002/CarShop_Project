17fe24f27d521f0a24c3f9dc828e4dbd94735e47
Vypboy <vytran210402@gmail.com>
2023-12-14 11:17:17 +0700

    update add_admin page

--- BE/Model/registerAdmin-data.php
+++ BE/Model/registerAdmin-data.php
@@ -1,15 +1,45 @@
 <?php
+    // header("Access-Control-Allow-Origin: http://localhost:3000");
+    // header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    // header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    // // Kết nối đến database
+    // $servername = "localhost";
+    // $username = "root";
+    // $password = "";
+    // $database = "shop";
+
+    // $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+    // $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // $data = json_decode(file_get_contents('php://input'), true);
+    // $id = $data['user_id'];
+    // $name = $data['user_name'];
+    // $email = $data['user_email'];
+    // $password = $data['user_password'];
+    // $birth = $data['user_birthday'];
+    // $address = $data['user_address'];
+    // $avatar = 'any';
+    // $phone = $data['user_phonenumber'];
+    // $question = $data['user_question'];
+    // $answer = $data['user_answer'];
+    // $state = 'true';
+    // $role = 'admin';
+
+    // $stmt = $conn->query("INSERT INTO account (id, name, email, password, birthday, address, avatar, phone_number, security_question, security_answer,state, role)
+    // VALUES ('$id','$name', '$email', '$password', '$birth', '$address','$avatar', '$phone', '$question', '$answer','$state','$role')");
+
     header("Access-Control-Allow-Origin: http://localhost:3000");
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    
     // Kết nối đến database
     $servername = "localhost";
     $username = "root";
     $password = "";
     $database = "shop";
-
+    
     $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
     $data = json_decode(file_get_contents('php://input'), true);
     $id = $data['user_id'];
     $name = $data['user_name'];
@@ -23,21 +53,30 @@
     $answer = $data['user_answer'];
     $state = 'true';
     $role = 'admin';
-
-
-    // Truy vấn để lấy giá trị max_id
-    $sql = "SELECT MAX(id) AS max_id FROM account";
+    
+    $sql = "INSERT INTO account (id, name, email, password, birthday, address, avatar, phone_number, security_question, security_answer, state, role)
+        VALUES (:id, :name, :email, :password, :birth, :address, :avatar, :phone, :question, :answer, :state, :role)";
+    
     $stmt = $conn->prepare($sql);
-    $stmt->execute();
+    $stmt->execute([
+        ':id' => $id,
+        ':name' => $name,
+        ':email' => $email,
+        ':password' => $password,
+        ':birth' => $birth,
+        ':address' => $address,
+        ':avatar' => $avatar,
+        ':phone' => $phone,
+        ':question' => $question,
+        ':answer' => $answer,
+        ':state' => $state,
+        ':role' => $role
+    ]);
+    // Fetch data as associative array
+    $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    // Lưu giá trị max_id vào biến $max_id
-    $result = $stmt->fetch(PDO::FETCH_ASSOC);
-    $max_id = $result['max_id'];
-
-
-
-    $stmt = $conn->query("INSERT INTO account (id,name, email, password, birthday, address, avatar, phone_number, security_question, security_answer,state, role)
-    VALUES ('$id','$name', '$email', '$password', '$birth', '$address','$avatar', '$phone', '$question', '$answer','$state','$role')
-    WHERE id = '$max_id' + 1");
-
+    // Output data as JSON
+    header('Content-Type: application/json');
+    echo json_encode($data);
 ?>
+    
